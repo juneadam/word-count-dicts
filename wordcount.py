b6c39a36bcf55cd4eba02efe49a4bc518911f55f
June Balter <junedoeswork@gmail.com>
2022-12-19 14:45:57 -0600

    completed file

--- wordcount.py
+++ wordcount.py
@@ -1,4 +1,30 @@
 """Count words in file."""
 
+file = open("twain.txt")
 
-# put your code here.
+dict = {}
+
+words = []
+
+for line in file:
+    for word in line.split():
+        words.append(word)
+
+
+# for word in words:
+#     if word in dict:
+#         dict[word] += 1
+#     else:
+#         dict[word] = 1
+
+for word in words:
+    dict[word] = dict.get(word, 0) + 1
+
+print(dict)
+
+
+
+# dict = {}
+# for line in file:
+#      for word in line.split():
+#         dict[word] = dict.get(word,0) + 1
